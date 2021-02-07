946840f842637ccfeae4047098bd479ec9900e61
heba-ali2030 <heba.galaa@gmail.com>
2021-02-07 23:01:04 +0200

    add

---
+++ Hossam.py
@@ -0,0 +1,15 @@
+#who are you for better future
+name =input('my name is ')
+age = input('I am  ') 
+print('years old')
+hobbies =input('my favourite hobby is ')
+
+other =input('do you have any other hobbies?')
+
+no = 3
+print(f' I have more than {no} good hobbies')
+strength =input('what is your strength points?')
+percent= input('my strength percent is nearly ')
+weak =input('what is your weak points?')
+percent= input('my weak percent is nearly ')
+print(f' as you have {hobbies} and {other}, you can be big Ramy')
