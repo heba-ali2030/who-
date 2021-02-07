5632a8afee3f8caa541ba6400244bcf1f8fb5db4
heba-ali2030 <heba.galaa@gmail.com>
2021-02-07 23:18:16 +0200

    add

--- heba.py
+++ heba.py
@@ -2,14 +2,17 @@
 name =input('my name is ')
 age = input('I am  ') 
 print('years old')
-hobbies =input('my favourite hobby is ')
 
+hobbies =input('my favourite hobby is ')
 other =input('do you have any other hobbies?')
 
-no = 3
-print(f' I have more than {no} good hobbies')
+no = input('what is your hobbies no?')
+print(f' actually I have nearly more than {no} good hobbies')
+
 strength =input('what is your strength points?')
 percent= input('my strength percent is nearly ')
+
 weak =input('what is your weak points?')
 percent= input('my weak percent is nearly ')
+
 print(f' as you have {hobbies} and {other}, you can go Zumba for fitness hahahaha')
